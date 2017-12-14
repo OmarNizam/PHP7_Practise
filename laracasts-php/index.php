20f2e886761bd40270ac0e7260bd9c2860902ac8
OmarNizam <eng.omarnizam@gmail.com>
2017-12-14 12:36:55 +0100

    Build a Router from scratch

--- laracasts-php/index.php
+++ laracasts-php/index.php
@@ -1,12 +1,6 @@
 <?php
 
-$query = require 'bootstrap.php';
+require 'core/bootstrap.php';
 
-require 'Task.php';
-
-
-$tasks = $query->selectAll('todos');
-
-
-
-require 'index.view.php';
+require Router::load('routes.php')
+   ->direct(Request::uri());
