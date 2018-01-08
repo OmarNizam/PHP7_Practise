5091f35dddd58d07051635321514633d5e0f37d2
OmarNizam <eng.omarnizam@gmail.com>
2018-01-09 00:21:32 +0100

    using $this variable

--- php-oop/class_and_object/index.php
+++ php-oop/class_and_object/index.php
@@ -17,11 +17,27 @@
     public $inch = '4 Inch';
     public $space = '16GB';
     public $color = 'Silver';
+    public $ownerName;
   // Methods
 
     public function doubleHomePressed()
     {
-      echo "You have pressed the home button twice";
+      echo "You have pressed the home button twice <br />";
+    }
+
+    public function getSpecification()
+    {
+      echo "This iPhone Ram Is: " . $this->ram . '<br />';
+      echo "This iPhone Screen size Is: " . $this->inch . '<br />';
+    }
+
+    public function setOwnerName()
+    {
+      if (strlen($this->ownerName) < 3) {
+        echo "Owner Name Canot Be Less Than 3 Chars !!!";
+      } else {
+        echo "Yor name has been set";
+      }
     }
   }
 
@@ -32,7 +48,10 @@
   $iphone6plus->inch = '5 Inch';
   $iphone6plus->space = '32GB';
   $iphone6plus->color = 'Gold';
+  $iphone6plus->ownerName = 'Omar';
   $iphone6plus->doubleHomePressed();   // calling the class method on the object
+  $iphone6plus->getSpecification();
+  $iphone6plus->setOwnerName();
   echo '<pre>';
     var_dump($iphone6plus);
   echo '</pre>';
