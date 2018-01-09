6ad14b7f8bc66452bc86ddc8262202baa0e018ac
OmarNizam <eng.omarnizam@gmail.com>
2018-01-09 15:06:35 +0100

    Modify data in Inheritance system

--- php-oop/Inheritance/index.php
+++ php-oop/Inheritance/index.php
@@ -2,13 +2,15 @@
 
 
 
-  class AppleDevice
+  class AppleDevice    // super class | perant class
   {
   // properties
     public $ram = '1GB';
     public $inch = '4 Inch';
     public $space = '16GB';
     public $color = 'Silver';
+    public $screen = 'LCD';
+    public $name;
 
 // Methods
     public function changeSpac($ra, $in, $sp, $co)
@@ -19,15 +21,26 @@
       $this->color = $co;
     }
 
+    public function sayHello($n)
+    {
+      $this->name = $n;
+      echo "Welcom to " . $n;
+    }
+
   }
 
-  class Sony extends AppleDevice
+  class Sony extends AppleDevice    // sub class | child class
   {
   // properties
 
     public $camera = '25MB';
 
 // Methods
+  public function sayHello($n)
+  {
+    $this->name = $n;
+    echo "Welcom to " . $n . ' this phone has ' . $this->ram . ' Ram.';
+  }
 
   }
 
@@ -35,6 +48,7 @@
 // iphone6plus Object
   $iphone6plus = new AppleDevice();
   $iphone6plus->changeSpac('2GB', '5 Inch', '32GB', 'Gold');
+  $iphone6plus->sayHello('iPhone');
   echo '<pre>';
     var_dump($iphone6plus);
   echo '</pre>';
@@ -42,6 +56,7 @@
   // sony Object
     $sony = new Sony();
     $sony->changeSpac('2GB', '5 Inch', '32GB', 'Gold');
+    $sony->sayHello('Sony');
     echo '<pre>';
       var_dump($sony);
     echo '</pre>';
